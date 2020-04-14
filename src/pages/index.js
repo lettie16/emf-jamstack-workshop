39bd232286eb5e77bf3a33914c8c1e7ae7d6efc3
lettie16 <mark.lett@ellenmacarthurfoundation.org>
2020-04-14 14:48:35 +0100

    little change

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ const Index = () => {
   return (
     <>
       <h1>EMF CI/CD Workshop</h1>
+      <p>Lets change stuff!</p>>
       <p>This is the project for our workshop on Gatsby Cloud and GitHub Actions</p>
       <p>Use this project for the workshop</p>
     </>
